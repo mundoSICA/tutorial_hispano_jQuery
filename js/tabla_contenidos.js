792a2267127f717693eca0fc1083e80ed299dd12
Fitorec <chanerec@gmail.com>
2012-07-06 12:53:28 -0500

    Agregando link hacia la pagina del ADSL, editando img logoADSL.

--- js/tabla_contenidos.js
+++ js/tabla_contenidos.js
@@ -1,3 +1,56 @@
+$(function() {
+tablaContenido = (function(div_contenedor, div_destino, tag_titulos){
+	//////////////////////////////////////////////////////////////////////////////////////
+	var root = '/tutorial_hispano_jQuery/',
+			$content = $(div_contenedor),
+			$destino = $content.find('asfds'),
+			$tagTitulos = $content.find(tag_titulos),
+			level = 2,
+			modo = 'sesion';
+	debug = function (){
+		console.log(root);
+		console.log($content);
+		console.log($destino);
+		console.log($tagTitulos);
+		console.log(document.title);
+		console.log('modo');
+	}
+	/* Setea la variable modo a partir del la ubicación de la página,
+	 * tomando los siguientes valores.
+	 *  index      - Pertenece a la pagina principal(index)
+	 *  sesion     - Pertenece a una sesión especifica.
+	 *  ej.index   - Index de ejemplos.
+	 *  ejemplo    - Algun ejemplo.
+	 *  re.index   - Es la sección de ejemplos o de recursos.
+	 *  recurso   -  Algun recurso.
+	 */
+	obtenerModo = function (value){
+		var path = window.location+'';
+		//optenemos la subcadena de la url.
+		path = path.substring( path.indexOf(root) + root.length );
+		//optenemos el nivel de profundidad respecto al index.
+		nivel = path.split('/').length;
+		//
+		if( nivel < 2 ){//Modo index el directorio es raiz.
+			modo = 'index';
+		}else if( n=path.match(/.*recursos\/(index.html)?$/g) ){
+			modo = 're.index';
+		}else if( n=path.match(/.*recursos/g) ){
+			modo = 'recurso';
+		}else if( n=path.match(/.*ejemplos\/(index.html)?$/g) ){
+			modo = 'ej.index';
+		}else if( n=path.match(/.*ejemplos/g) ){
+			modo = 'ejemplo';
+		}
+		//alert( modo + ' nivel:' + nivel );
+		return path;
+	}
+	debug();
+	modo = obtenerModo();
+})('#content', '#tablas_contenidos', 'h2');
+});
+
+
 /* Se encarga de generar una lista de contendidos a partir de los títulos nivel 2
  * encontrados dentro de un div identificado como 'content'.
  **/
