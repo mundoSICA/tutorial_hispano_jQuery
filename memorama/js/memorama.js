c7c951802e657a5ef5b3cdca7e82212f6bf61348
Fitorec <chanerec@gmail.com>
2013-05-16 19:11:07 -0500

    Revision del memorama.
    Programacion orientada a prototipos.

--- memorama/js/memorama.js
+++ memorama/js/memorama.js
@@ -9,14 +9,19 @@ function Memorama(tabledo, options) {
 	this.options = $.extend({}, this.defaultOptions, options);
 	this.iniciarTablero();
 }
-// Opciones por defectos
+/*
+ *  Opciones por defecto
+ */
 Memorama.prototype.defaultOptions = {
 	tema: "one-piece",
+	temaNombre: "One Piece",
 	numFichas: 12,
 	clickNumero: 0,
 	ultimoTipoFicha: 0,
 };
-// Este metodo se encarga de inicializar el tablero.
+/*
+ *  Se encarga de inicializar el tablero.
+ */
 Memorama.prototype.iniciarTablero = function() {
 	//Borramos y creamos el tablero
 	this.borrarTablero();
@@ -32,9 +37,12 @@ Memorama.prototype.iniciarTablero = function() {
 	}
 	//Agregamos el evento del click
 	memorama = this;
-	this.$tablero.find('.ficha').click(function (){
+	this.$tablero.find('.ficha').click(function () {
 		memorama.clickFicha($(this));
    });
+   this.$tablero.append(
+		'<a href="https://github.com/mundoSICA/tutorial_hispano_jQuery/tree/gh-pages/memorama" target="_blank">Explorar código fuente</a>'
+		);
 }
 
 Memorama.prototype.clickFicha = function ($ficha) {
@@ -45,9 +53,14 @@ Memorama.prototype.clickFicha = function ($ficha) {
 	this.options.clickNumero = (this.options.clickNumero + 1) % 2;
 	if( this.options.clickNumero == 0 ) {
 		$ultimasFichas = this.$tablero.find('[data-tipo="'+this.options.ultimoTipoFicha+'"]');
-		if( this.options.ultimoTipoFicha == tipo ) {
-			$ultimasFichas.animate({'opacity': 0.5}, 2000);
-		} else {
+		this.incrementarIntentos();
+		if( this.options.ultimoTipoFicha == tipo ) { //El usuario le acerto
+			$ultimasFichas.animate({'opacity': 0.3}, 2000);
+			this.incrementarFichasAcertadas();
+			if( $numFichasAcertadas.text() == this.options.numFichas ){
+				alert("Felicidades acabaste en " + $numIntentos.text() +" Intentos" );
+			}
+		} else { //El usuario no acerto
 			setTimeout(function() {
 				$ultimasFichas.css('backgroundPosition', "0 0");
 				$ficha.css('backgroundPosition', "0 0");
@@ -56,6 +69,20 @@ Memorama.prototype.clickFicha = function ($ficha) {
 	} else {
 		this.options.ultimoTipoFicha = tipo;
 	}
+}
+/*
+ *  Incrementa en 1 el número de intentos
+ */
+Memorama.prototype.incrementarIntentos = function() {
+	$numIntentos = this.$tablero.find('.numIntentos');
+	$numIntentos.text(1* $numIntentos.text() + 1);
+}
+/*
+ * Incrementa en 2 el número de fichas Acertadas
+ */
+Memorama.prototype.incrementarFichasAcertadas = function() {
+	$numFichasAcertadas = this.$tablero.find('.numFichasAcertadas');
+	$numFichasAcertadas.text(1* $numFichasAcertadas.text() + 2);
 }
 /* Regresa el tema actual
  */
@@ -71,10 +98,6 @@ Memorama.prototype.borrarTablero = function (tema) {
 	this.$tablero.find('.ficha').remove();
 }
 
-/*Memorama.prototype.tema = function (tema) {
-	this.options.tema = tema;
-}*/
-
-jQuery(function (){
-	memorama = new Memorama('#tablero', {});
+jQuery(function () {
+	memorama = new Memorama('#tablero', {numFichas: 16});
 });
